225744808fc2e390fe0ed79b36f8256ca93c2064
Iqbal <iqbalchoudhury2@gmail.com>
2017-09-03 03:31:20 -0400

    pom update and test file loaded

--- src/main/java/appiumFramework/appium_framework/CalcAppTest.java
+++ src/main/java/appiumFramework/appium_framework/CalcAppTest.java
@@ -1,5 +1,31 @@
 package appiumFramework.appium_framework;
 
-public class CalcAppTest {
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class CalcAppTest {
+	AppiumDriver <WebElement>driver;
+	
+	@BeforeClass
+	public void setUp() throws MalformedURLException {
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "donatello");
+		cap.setCapability("appPackage", "APP_PACKAGE");
+		cap.setCapability("appActivity", "appActivity");
+		driver=new AndroidDriver <WebElement>(new URL(""),cap);
+	}
+	
+	@Test
+	public void SimpleTest() {
+		
+	}
 }
--- src/test/java/appiumFramework/appium_framework/CalcAppPage.java
+++ src/test/java/appiumFramework/appium_framework/CalcAppPage.java
@@ -1,5 +1,29 @@
 package appiumFramework.appium_framework;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
 public class CalcAppPage {
+	
+	@AndroidBy(id="editNo1")
+	public WebElement txtNUm1;
+	
+	@AndroidBy(id="editNo2")
+	public WebElement txtNUm2;
+	
+	
+	@AndroidBy(id="txtResult")
+	public WebElement txtResult;
+
+
+	public CalcAppPage(AndroidDriver <WebElement> driver) {
+		PageFactory.initElements(new AppiumFieldDecorator(driver),this);
+	}
+	
+	
 
 }
